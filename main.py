4da0840e4396cfd1234637595ef55e4b0ed98c61
root <root@kali>
2025-06-20 11:48:56 +0300

    works!

--- main.py
+++ main.py
@@ -587,6 +587,7 @@ async def finish_questionnaire(update: Update, context: CallbackContext) -> int:
             chat_id=update.message.chat_id,
             user_id=user_id,
             name=str(user_id)
+            )
         print(f"Создана задача напоминаний для пользователя {user_id}")
     
     if language == "ru":
