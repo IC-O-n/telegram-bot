05d8097c6a7fe376346b54cff27ed8600f4a40df
root <root@kali>
2025-11-09 13:28:26 +0300

    works!

--- main.py
+++ main.py
@@ -5300,7 +5300,7 @@ TEXT: ...
                     print(f"Ошибка при сохранении данных о приеме пищи: {e}")
 
         if sticker_message:
-            await asyncio.sleep(1)
+            await asyncio.sleep(2)
             try:
                 await sticker_message.delete()
             except Exception as e:
